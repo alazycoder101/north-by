0ecd259747a556812549d9448992a6c4fce977bd
lazycoder <alazycoder101@gmail.com>
2021-08-12 18:33:17 +0100

    add controller and view for converts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+#---
+# Excerpted from "Modern Front-End Development for Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/nrclient for more book information.
+#---
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources(:concerts)
+  devise_for(:users)
 end
