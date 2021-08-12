a4040c63aec95a821bfd0208860c669ef76fd010
lazycoder <alazycoder101@gmail.com>
2021-08-12 17:39:04 +0100

    update with tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,11 @@
+/***
+ * Excerpted from "Modern Front-End Development for Rails",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit http://www.pragmaticprogrammer.com/titles/nrclient for more book information.
+***/
 module.exports = {
   plugins: [
     require('postcss-import'),
@@ -7,6 +15,8 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
+    require('tailwindcss'),
+    require('autoprefixer')
   ]
 }
